1903dcf569cda92831a046bd8570944c116e5c60
dave <david.feer04@gmail.com>
2026-05-20 00:11:25 +0200

    feat: updated root layout

--- autofinder/src/routes/+layout.svelte
+++ autofinder/src/routes/+layout.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
 	import '../app.css';
 	import Nav from '$lib/components/Nav.svelte';
+	import { page } from '$app/stores';
 
 	let { children } = $props();
+
+	let isWizard = $derived(
+		$page.url.pathname.startsWith('/finder') || $page.url.pathname === '/berechnung'
+	);
 </script>
 
-<Nav />
-<main class="min-h-[calc(100vh-4rem)] bg-gray-50">
+{#if isWizard}
 	{@render children()}
-</main>
+{:else}
+	<Nav />
+	<main class="min-h-[calc(100vh-4rem)] bg-gray-50">
+		{@render children()}
+	</main>
+{/if}
